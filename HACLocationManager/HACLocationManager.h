fff6815f89377756fdc02041bdc007b489fb8301
litoarias <lito.arias.cervero@gmail.com>
2016-10-29 14:34:59 +0200

    Add framework for carthage

---
+++ HACLocationManager/HACLocationManager.h
@@ -0,0 +1,19 @@
+//
+//  HACLocationManager.h
+//  HACLocationManager
+//
+//  Created by Hipolito Arias on 29/10/16.
+//  Copyright © 2016 Hipolito Arias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HACLocationManager.
+FOUNDATION_EXPORT double HACLocationManagerVersionNumber;
+
+//! Project version string for HACLocationManager.
+FOUNDATION_EXPORT const unsigned char HACLocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HACLocationManager/PublicHeader.h>
+
+
