a59cf9c4cf6aa2935c58298275dd6334186811a9
litoarias <lito.arias.cervero@gmail.com>
2016-10-29 13:37:26 +0200

    Shared schema for carthage

--- ExampleApp/AppDelegate.h
+++ ExampleApp/AppDelegate.h
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.h
-//  LocationManager
-//
-//  Created by Hipolito Arias on 25/3/15.
-//  Copyright (c) 2015 Hipolito Arias. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
